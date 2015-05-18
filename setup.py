30b9b237fc67914a8a5660ca8352708fd3ec2b2f
Phil Ngo <ngo.phil@gmail.com>
2015-05-18 15:43:35 -0500

    add basic setup.py and version

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+version = __import__('thermostat').get_version()
+
+long_description = "Calculate connected thermostat temperature/run-time savings."
+
+setup(name='thermostat',
+    version=version,
+    description='Calculate connected thermostat savings',
+    long_description=long_description,
+    url='https://github.com/impactlab/thermostat/',
+    author='Phil Ngo',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+    ],
+    keywords='thermostat savings',
+    packages=find_packages(),
+    install_requires=[],
+)
