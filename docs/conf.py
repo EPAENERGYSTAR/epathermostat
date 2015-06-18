7bcc00d5498fa7526df58cc4b0b25c138c724451
Phil Ngo <ngo.phil@gmail.com>
2015-06-18 13:28:57 -0500

    mock eemeter

--- docs/conf.py
+++ docs/conf.py
@@ -26,6 +26,9 @@ class Mock(MagicMock):
 
 MOCK_MODULES = [
     'pandas',
+    'eemeter',
+    'eemeter.consumption',
+    'eemeter.weather',
 ]
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
