069e5a9082e46b70a3b1cce6ba12855e5dd44c2d
Phil Ngo <ngo.phil@gmail.com>
2015-06-22 18:29:38 -0500

    add demand calculations for HDD and CDD using options 1 and 2

--- tests/test_demand.py
+++ tests/test_demand.py
@@ -51,3 +51,100 @@ def test_get_heating_demand_deltaT(valid_thermostat_id,valid_temperature_setpoin
     deltaT = get_heating_demand(thermostat_type_2,heating_season,method="deltaT")
     assert_allclose(deltaT,np.tile(20,(400,)),rtol=RTOL,atol=ATOL)
 
+def test_get_cooling_demand_dailyavgCDD(valid_thermostat_id,valid_datetimeindex):
+    temp_in = pd.Series(np.tile(70,(400,)),index=valid_datetimeindex)
+    temp_out = pd.Series(np.linspace(80,90,num=400),index=valid_datetimeindex)
+
+    hourly_alpha = 20
+    daily_alpha = hourly_alpha * 24
+
+    ss_heat_pump_cooling = pd.Series(np.maximum((temp_out - temp_in) * hourly_alpha,0),index=valid_datetimeindex)
+    ss_heat_pump_heating = pd.Series(np.tile(0,(400,)),index=valid_datetimeindex)
+
+    thermostat_type_2 = Thermostat(valid_thermostat_id,2,temp_in,valid_temperature_setpoint,temp_out,
+            ss_heat_pump_cooling=ss_heat_pump_cooling,ss_heat_pump_heating=ss_heat_pump_heating)
+
+    cooling_season, name = thermostat_type_2.get_cooling_seasons()[0]
+    dailyavgCDD, deltaT_base_estimate, alpha_estimate, error = \
+            get_cooling_demand(thermostat_type_2,cooling_season,method="dailyavgCDD",column_name="ss_heat_pump_cooling")
+
+    assert_allclose(dailyavgCDD,[ 9.899, 10.501, 11.102, 11.704, 12.305,
+                                  12.907, 13.508, 14.110, 14.711, 15.313,
+                                  15.914, 16.516, 17.117, 17.719, 18.320,
+                                  18.922, 19.423],rtol=0.01,atol=0.01)
+    assert_allclose(deltaT_base_estimate, 70, rtol=1, atol=.01)
+    assert_allclose(alpha_estimate, daily_alpha, rtol=RTOL, atol=ATOL)
+    assert error < 50000
+
+def test_get_heating_demand_dailyavgHDD(valid_thermostat_id,valid_datetimeindex):
+    temp_in = pd.Series(np.tile(70,(400,)),index=valid_datetimeindex)
+    temp_out = pd.Series(np.linspace(60,50,num=400),index=valid_datetimeindex)
+
+    hourly_alpha = 20
+    daily_alpha = hourly_alpha * 24
+
+    ss_heat_pump_cooling = pd.Series(np.tile(0,(400,)),index=valid_datetimeindex)
+    ss_heat_pump_heating = pd.Series(np.maximum((temp_in - temp_out) * hourly_alpha,0),index=valid_datetimeindex)
+
+    thermostat_type_2 = Thermostat(valid_thermostat_id,2,temp_in,valid_temperature_setpoint,temp_out,
+            ss_heat_pump_cooling=ss_heat_pump_cooling,ss_heat_pump_heating=ss_heat_pump_heating)
+
+    heating_season, name = thermostat_type_2.get_heating_seasons()[0]
+    dailyavgHDD, deltaT_base_estimate, alpha_estimate, error = \
+            get_heating_demand(thermostat_type_2,heating_season,method="dailyavgHDD",column_name="ss_heat_pump_heating")
+
+    assert_allclose(dailyavgHDD,[ 9.899, 10.501, 11.102, 11.704, 12.305,
+                                  12.907, 13.508, 14.110, 14.711, 15.313,
+                                  15.914, 16.516, 17.117, 17.719, 18.320,
+                                  18.922, 19.423],rtol=0.01,atol=0.01)
+    assert_allclose(deltaT_base_estimate, 70, rtol=1, atol=.01)
+    assert_allclose(alpha_estimate, daily_alpha, rtol=RTOL, atol=ATOL)
+    assert error < 50000
+
+def test_get_cooling_demand_hourlysumCDD(valid_thermostat_id,valid_datetimeindex):
+    temp_in = pd.Series(np.tile(70,(400,)),index=valid_datetimeindex)
+    temp_out = pd.Series(np.linspace(80,90,num=400),index=valid_datetimeindex)
+
+    hourly_alpha = 20
+
+    ss_heat_pump_cooling = pd.Series(np.maximum((temp_out - temp_in) * hourly_alpha,0),index=valid_datetimeindex)
+    ss_heat_pump_heating = pd.Series(np.tile(0,(400,)),index=valid_datetimeindex)
+
+    thermostat_type_2 = Thermostat(valid_thermostat_id,2,temp_in,valid_temperature_setpoint,temp_out,
+            ss_heat_pump_cooling=ss_heat_pump_cooling,ss_heat_pump_heating=ss_heat_pump_heating)
+
+    cooling_season, name = thermostat_type_2.get_cooling_seasons()[0]
+    hourlysumCDD, deltaT_base_estimate, alpha_estimate, error = \
+            get_cooling_demand(thermostat_type_2,cooling_season,method="hourlysumCDD",column_name="ss_heat_pump_cooling")
+
+    assert_allclose(hourlysumCDD,[ 8.239, 8.841, 9.442, 10.044, 10.645,
+                                   11.247, 11.848, 12.45, 13.051, 13.653,
+                                   14.254, 14.856, 15.457, 16.059, 16.660,
+                                   17.262, 11.842], rtol=0.01, atol=0.01)
+    assert_allclose(deltaT_base_estimate, 70, rtol=1, atol=.01)
+    assert_allclose(alpha_estimate, 555.918, rtol=0.01, atol=0.01)
+    assert error < 550000
+
+def test_get_heating_demand_hourlysumHDD(valid_thermostat_id,valid_datetimeindex):
+    temp_in = pd.Series(np.tile(70,(400,)),index=valid_datetimeindex)
+    temp_out = pd.Series(np.linspace(60,50,num=400),index=valid_datetimeindex)
+
+    hourly_alpha = 20
+
+    ss_heat_pump_cooling = pd.Series(np.tile(0,(400,)),index=valid_datetimeindex)
+    ss_heat_pump_heating = pd.Series(np.maximum((temp_in - temp_out) * hourly_alpha,0),index=valid_datetimeindex)
+
+    thermostat_type_2 = Thermostat(valid_thermostat_id,2,temp_in,valid_temperature_setpoint,temp_out,
+            ss_heat_pump_cooling=ss_heat_pump_cooling,ss_heat_pump_heating=ss_heat_pump_heating)
+
+    heating_season, name = thermostat_type_2.get_heating_seasons()[0]
+    hourlysumHDD, deltaT_base_estimate, alpha_estimate, error = \
+            get_heating_demand(thermostat_type_2,heating_season,method="hourlysumHDD",column_name="ss_heat_pump_heating")
+
+    assert_allclose(hourlysumHDD,[ 8.239, 8.841, 9.442, 10.044, 10.645,
+                                   11.247, 11.848, 12.45, 13.051, 13.653,
+                                   14.254, 14.856, 15.457, 16.059, 16.660,
+                                   17.262, 11.842], rtol=0.01, atol=0.01)
+    assert_allclose(deltaT_base_estimate, 70, rtol=1, atol=.01)
+    assert_allclose(alpha_estimate, 555.918, rtol=0.01, atol=0.01)
+    assert error < 550000
--- thermostat/demand.py
+++ thermostat/demand.py
@@ -1,4 +1,7 @@
-def get_cooling_demand(thermostat,cooling_season,method="deltaT"):
+import numpy as np
+from scipy.optimize import leastsq
+
+def get_cooling_demand(thermostat,cooling_season,method="deltaT",column_name=None):
     """
     Calculates a measure of cooling demand.
 
@@ -14,6 +17,10 @@ def get_cooling_demand(thermostat,cooling_season,method="deltaT"):
         The method to use during calculation of demand.
 
         - "deltaT": :math:`\Delta T = temp_{in} - temp_{out}`
+    column_name : str
+        The name of the column for which to calculate demand estimates which use
+        runtime to optimize CDD base temperature ("hourlysumCDD" and
+        "dailyavgCDD").
 
     Returns
     -------
@@ -21,19 +28,53 @@ def get_cooling_demand(thermostat,cooling_season,method="deltaT"):
         Demand at each hour in the heating season as calculated using one of
         the supported methods.
     """
+    season_temp_in = thermostat.temperature_in[cooling_season]
+    season_temp_out = thermostat.temperature_out[cooling_season]
+    season_deltaT = season_temp_in - season_temp_out
+
     if method == "deltaT":
-        season_temp_in = thermostat.temperature_in[cooling_season]
-        season_temp_out = thermostat.temperature_out[cooling_season]
-        demand = season_temp_in - season_temp_out
-        return demand
-    elif method == "hourlysumCDD":
-        raise NotImplementedError
+        return season_deltaT
     elif method == "dailyavgCDD":
-        raise NotImplementedError
+        daily_avg_deltaT = np.array([-temps.mean() for day, temps in season_deltaT.groupby(season_deltaT.index.date)])
+        def calc_cdd(deltaT_base):
+            return np.maximum(daily_avg_deltaT - deltaT_base,0)
+    elif method == "hourlysumCDD":
+        def calc_cdd(deltaT_base):
+            hourly_cdd = (-season_deltaT - deltaT_base).apply(lambda x: np.maximum(x,0))
+            return np.array([cdd.sum() / 24 for day, cdd in hourly_cdd.groupby(season_deltaT.index.date)])
     else:
         raise NotImplementedError
 
-def get_heating_demand(thermostat,heating_season,method="deltaT"):
+    cooling_column = thermostat.__dict__.get(column_name)
+    cooling_runtime = cooling_column[cooling_season]
+
+    # calculate daily runtimes by taking the average hourly then multiplying by 24
+    # - this attempts to account for partial days (which may occur at endpoints)
+    daily_runtime = np.array([runtimes.mean() * 24 for day, runtimes in cooling_runtime.groupby(cooling_runtime.index.date)])
+    total_runtime = cooling_runtime.sum()
+
+    def calc_estimates(deltaT_base):
+        cdd = calc_cdd(deltaT_base)
+        total_cdd = np.sum(cdd)
+        alpha_estimate = total_runtime / total_cdd
+        runtime_estimate = cdd * alpha_estimate
+        errors = daily_runtime - runtime_estimate
+        return cdd, alpha_estimate, errors
+
+    def estimate_errors(deltaT_base_estimate):
+        _,_,errors = calc_estimates(deltaT_base_estimate)
+        return errors
+
+    deltaT_base_starting_guess = 0
+    y, _ = leastsq(estimate_errors, deltaT_base_starting_guess)
+    deltaT_base_estimate = y[0]
+
+    cdd, alpha_estimate, errors = calc_estimates(deltaT_base_estimate)
+    mean_squared_error = np.mean((errors)**2)
+
+    return cdd, deltaT_base_estimate, alpha_estimate, mean_squared_error
+
+def get_heating_demand(thermostat,heating_season,method="deltaT",column_name=None):
     """
     Calculates a measure of heating demand.
 
@@ -49,6 +90,10 @@ def get_heating_demand(thermostat,heating_season,method="deltaT"):
         The method to use during calculation of demand.
 
         - "deltaT": :math:`\Delta T = temp_{in} - temp_{out}`
+    column_name : str
+        The name of the column for which to calculate demand estimates which use
+        runtime to optimize HDD base temperature ("hourlysumHDD" and
+        "dailyavgHDD").
 
     Returns
     -------
@@ -56,14 +101,48 @@ def get_heating_demand(thermostat,heating_season,method="deltaT"):
         Demand at each hour in the heating season as calculated using one of
         the supported methods.
     """
+    season_temp_in = thermostat.temperature_in[heating_season]
+    season_temp_out = thermostat.temperature_out[heating_season]
+    season_deltaT = season_temp_in - season_temp_out
+
     if method == "deltaT":
-        season_temp_in = thermostat.temperature_in[heating_season]
-        season_temp_out = thermostat.temperature_out[heating_season]
-        demand = season_temp_in - season_temp_out
-        return demand
-    elif method == "hourlysumHDD":
-        raise NotImplementedError
+        return season_deltaT
     elif method == "dailyavgHDD":
-        raise NotImplementedError
+        daily_avg_deltaT = np.array([temps.mean() for day, temps in season_deltaT.groupby(season_deltaT.index.date)])
+        def calc_hdd(deltaT_base):
+            return np.maximum(daily_avg_deltaT - deltaT_base,0)
+    elif method == "hourlysumHDD":
+        def calc_hdd(deltaT_base):
+            hourly_hdd = (season_deltaT - deltaT_base).apply(lambda x: np.maximum(x,0))
+            return np.array([hdd.sum() / 24 for day, hdd in hourly_hdd.groupby(season_deltaT.index.date)])
     else:
         raise NotImplementedError
+
+    heating_column = thermostat.__dict__.get(column_name)
+    heating_runtime = heating_column[heating_season]
+
+    # calculate daily runtimes by taking the average hourly then multiplying by 24
+    # - this attempts to account for partial days (which may occur at endpoints)
+    daily_runtime = np.array([runtimes.mean() * 24 for day, runtimes in heating_runtime.groupby(heating_runtime.index.date)])
+    total_runtime = heating_runtime.sum()
+
+    def calc_estimates(deltaT_base):
+        hdd = calc_hdd(deltaT_base)
+        total_hdd = np.sum(hdd)
+        alpha_estimate = total_runtime / total_hdd
+        runtime_estimate = hdd * alpha_estimate
+        errors = daily_runtime - runtime_estimate
+        return hdd, alpha_estimate, errors
+
+    def estimate_errors(deltaT_base_estimate):
+        _, _, errors = calc_estimates(deltaT_base_estimate)
+        return errors
+
+    deltaT_base_starting_guess = 0
+    y, _ = leastsq(estimate_errors, deltaT_base_starting_guess)
+    deltaT_base_estimate = y[0]
+
+    hdd, alpha_estimate, errors = calc_estimates(deltaT_base_estimate)
+    mean_squared_error = np.mean((errors)**2)
+
+    return hdd, deltaT_base_estimate, alpha_estimate, mean_squared_error
